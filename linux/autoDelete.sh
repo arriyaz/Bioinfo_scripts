34edfe84285650eb5e6255ca0d078ff1a60ceb65
Ar Riyaz <48599524+arriyaz@users.noreply.github.com>
2024-09-23 20:02:11 -0400

    Added codes to delete files from downloads.

--- linux/autoDelete.sh
+++ linux/autoDelete.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+
+
 # Clean everything in the temp directory older than 30 days
 find "$HOME/temp" -mindepth 1 -mtime +30 -depth -exec rm -rf {} \;
+
+# Clean old files from the Downloads folder (uncomment to activate the code)
+find "$HOME/Downloads" -mindepth 1 -mtime +40 -depth -exec rm -rf {} \;
